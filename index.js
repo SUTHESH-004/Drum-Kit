81388b5ff144d1315aa89fb074b72323e5341936
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-11 22:58:11 +0530

    used switch case instead of if else ladder

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-// // alert("YOu are a genius of adaptability");
-// var ab = document.querySelectorAll("button")[0];
-// ab.addEventListener("click",alerts);
-// function alerts(){
-//     alert("You are blue1");
-// };
-// alert("Suthesh");
-let n = document.querySelectorAll(".drum").length;
-console.log(n);
-for(let i=0;i<n;i++)
-{
-     let ab = document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-       alert("I got clicked");
-     } );
 
-     }
+let n = document.querySelectorAll(".drum").length;
+// console.log(n);
+for(var i=0;i<n;i++)
+{
+   var some;
+   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+   // some=console.log(this.innerHtml);
+   var c = this.innerHtml;
+   addsound(c);
+   });
+   // var buttoninnerhtml = this.innerhtml;
+   // makesound(buttoninnerhtml);
+}
+
+function addsound(s)
+{
+   switch(s){
+   case "w":
+      var w0 = new Audio("sounds/tom1-1.mp3");
+      w0.play();
+      break;
+   case "a":
+      var a0 = new Audio("sounds/tom-2.mp3");
+      a0.play();
+      break;
+   case "s":
+      var s0 = new Audio("sounds/tom-3.mp3");
+      s0.play();
+      break;
+   case "d":
+      var d0 = new Audio("sounds/tom-4.mp3");
+      d0.play();
+      break;
+   case "j":
+      var j0 = new Audio("sounds/crash.mp3");
+      j0.play();
+      break;
+   case "k":
+      var k0 = new Audio("sounds/snare.mp3");
+      k0.play();
+      break;
+   case "l":
+      var l0 = new Audio("sounds/kick-bass.mp3");
+      l0.play();
+      break;
+   // default:console.log(s);
+   }}
